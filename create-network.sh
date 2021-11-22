c67a99ab8cda30533e0ea5425f834e86aa7df4af
wesleyburlani <wesleyburlani@gmail.com>
2021-11-22 10:42:04 -0500

    refactor: refactor shell scripts

--- create-network.sh
+++ create-network.sh
@@ -1,7 +1,7 @@
-numberOfRouters=$(cat inputs/routers.config | grep  -v -e '^$' | wc --l | awk '{print $1}')
+number_of_routers=$(cat inputs/routers.config | grep  -v -e '^$' | wc --l | awk '{print $1}')
 
-for refLine in `seq 1 $numberOfRouters`
+for current_line in `seq 1 $number_of_routers`
 do
-    routerId=$(cat inputs/routers.config | head -n +$refLine | tail -n 1 | awk '{print $1;}')
-    ./create-router.sh $routerId
+    router_id=$(cat inputs/routers.config | head -n +$current_line | tail -n 1 | awk '{print $1;}')
+    ./create-router.sh $router_id
 done
--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,11 @@
+rm -Rf improvc
+mkdir improvc
+
+rm -Rf bin 
+mkdir bin
+
+sudo apt install -y gnome-terminal
+
 wget https://github.com/wesleyburlani/improvc/archive/master.zip
 unzip master.zip
 mv improvc-master/* improvc
